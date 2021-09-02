5ca75a61c393442191ce1ffeb82cf6ef9e122786
Dan <78874812+ymwjbxxq@users.noreply.github.com>
2021-09-02 16:02:43 +0200

    fix the build.sh adding the right platform

--- src/StopRunningStepFunctionExecution/build.sh
+++ src/StopRunningStepFunctionExecution/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dotnet publish ./StopRunningStepFunctionExecution.csproj --configuration "Release" --framework "netcoreapp3.1" /p:PublishReadyToRun=true /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64 --self-contained false
+dotnet publish ./StopRunningStepFunctionExecution.csproj --configuration "Release" --framework "netcoreapp3.1" /p:PublishReadyToRun=true /p:GenerateRuntimeConfigurationFiles=true --runtime linux-musl-x64 --self-contained false
 # Create a new directory where we will put deployment package
 mkdir -p /package
 # # Go to bin folder with built project
-cd ./bin/Release/netcoreapp3.1/publish
+cd ./bin/Release/netcoreapp3.1/linux-musl-x64/publish
 # # Archive deployment package and store it to the specified folder
 zip -9 -r /package/deployment-archive.zip . || exit 1
 # # # Create the output directory in the mounted volume
